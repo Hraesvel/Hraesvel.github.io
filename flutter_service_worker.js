abb88a3fd6a165ec2e4de3de6252c62a62fa6d82
Martin Smith <mcodesmith@gmail.com>
2021-05-04 13:18:52 -0700

    update to flutter project 1.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "061117f6b5c2056e937262fa4ec8796e",
-"assets/assets/experiences.json": "6ad38cdd15862c8b1e41f0466b0218fb",
+  "assets/AssetManifest.json": "4d0c111dae3c7d12f64d82ab22deb923",
+"assets/assets/experiences.json": "1d0d4b5c281a183565459cd8f0ebb03d",
 "assets/assets/fonts/noto/NotoMono-Regular.ttf": "22be61396f729519d373f6d32cf52436",
 "assets/assets/fonts/roboto/Roboto-Bold.ttf": "e07df86cef2e721115583d61d1fb68a6",
 "assets/assets/fonts/roboto/Roboto-Light.ttf": "88823c2015ffd5fa89d567e17297a137",
@@ -16,10 +16,11 @@ const RESOURCES = {
 "assets/assets/img/self_02.png": "0f8432e50ee09e2f39069ee6928362f1",
 "assets/assets/img/self_02_HD.png": "304bc9bd53eda133e59f0015ff59131f",
 "assets/assets/projects/projects.json": "7dc58bf9bc7c14f908444888782607a0",
+"assets/assets/projects/_projects.json": "7dc58bf9bc7c14f908444888782607a0",
 "assets/assets/text_template/about_me.mustache": "ad0702657ff4c34d323d4d35835a943f",
 "assets/FontManifest.json": "a6462b0006236eaf0faec8a533de812f",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "e11611878ea90fde1be02905ef209b01",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "e3365778b500f73ed94f3b7ac1bc2d5f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttericon/lib/fonts/Brandico.ttf": "791921e9b25210e2551b6eda3f86c733",
 "assets/packages/fluttericon/lib/fonts/Elusive.ttf": "23f24df0388819e94db2b3c19841841c",
@@ -39,15 +40,16 @@ const RESOURCES = {
 "assets/packages/fluttericon/lib/fonts/Typicons.ttf": "3386cae1128e52caf268508d477fb660",
 "assets/packages/fluttericon/lib/fonts/WebSymbols.ttf": "4fd66aa74cdc6e5eaff0ec916ac269c6",
 "assets/packages/fluttericon/lib/fonts/Zocial.ttf": "c29d6e34d8f703a745c6f18d94ce316d",
-"assets/static/Martin_Backend_Engineer.pdf": "7986a0bd3fe56c740996c00ca8f068a7",
+"assets/static/Martin_Backend_Engineer.pdf": "0a640b6a9ccaa6130e4b68850f2d28c2",
+"assets/static/Martin_Backend_Engineer.pdf.2019": "7986a0bd3fe56c740996c00ca8f068a7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "5906e4a9f8e2a211c2607af6ae423b7a",
-"/": "5906e4a9f8e2a211c2607af6ae423b7a",
-"main.dart.js": "521163735232e09ec7f58e1015f4f57a",
+"index.html": "7c25d361bd7672a1e193b0a84692811e",
+"/": "7c25d361bd7672a1e193b0a84692811e",
+"main.dart.js": "c92b8fdc81a500ebb2472480f65233c0",
 "manifest.json": "535ee30a5bcdd1e67e582b8dde9a4c6d",
-"version.json": "64ee6ef40507987ac3d2de2d2220cee7"
+"version.json": "15b7ebe12f8a4d25bac3005ec1034bb9"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -65,7 +67,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
